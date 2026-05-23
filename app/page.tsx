3e210c5f4e930c156cc4cde5b465d61bc48e655e
Macy Holbrook <macyholbrook@Mac.lan>
2026-05-23 12:20:36 -0400

    fix typescript error

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 
 export default function Home() {
